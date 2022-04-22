8d460a83ccc72ce430285886b548906953bcea46
mivis <mivis@tut.by>
2022-04-22 17:28:35 +0300

    Add files via upload

---
+++ task_14_action.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+if ($_POST) {
+    $email = $_POST['email'];  
+
+    //PDO
+    try {
+        $pdo = new PDO("mysql:host=localhost;dbname=marlin-edu","root","");
+    }
+    catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+
+    $sql = "SELECT * FROM users WHERE email=:email";
+    $statement = $pdo->prepare($sql);
+    $statement->execute(['email' => $email]);
+    $user = $statement->fetch(PDO::FETCH_ASSOC);    
+
+    //проверка на наличие email в БД
+    if ($user['email'] == $email) {
+        //проверка совпадения пароля
+        if (password_verify($_POST['password'], $user['password'])) {
+            $_SESSION['success'] = "Вход совершен";
+            header("Location: task_14.php");
+            exit;
+        }
+    }
+    //несовпадение email или password
+    $_SESSION['error'] = "Неверный логин или пароль";
+    header("Location: task_14.php");
+
+    
+}
+?>
